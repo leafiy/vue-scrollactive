efa45e71262a7a5c3ba26cc044ef31ac4748400f
root <root@dell.localdomain>
2018-05-13 02:45:22 +0800

    ignore non-exist element

--- src/scrollactive.vue
+++ src/scrollactive.vue
@@ -3,83 +3,82 @@
     <slot></slot>
   </nav>
 </template>
-
 <script>
 import bezierEasing from 'bezier-easing';
 
 export default {
   props: {
     /**
-    * Class that will be applied in the menu item.
-    *
-    * @default  'is-active'
-    * @type {String}
-    */
+     * Class that will be applied in the menu item.
+     *
+     * @default  'is-active'
+     * @type {String}
+     */
     activeClass: {
       type: String,
       default: 'is-active',
     },
 
     /**
-    * Amount of space between top of screen and the section to
-    * highlight. (Usually your fixed header's height)
-    *
-    * @default 20
-    * @type {Number}
-    */
+     * Amount of space between top of screen and the section to
+     * highlight. (Usually your fixed header's height)
+     *
+     * @default 20
+     * @type {Number}
+     */
     offset: {
       type: Number,
       default: 20,
     },
 
     /**
-    * Enables/disables the scrolling when clicking in a menu item.
-    * Disable if you'd like to handle the scrolling by your own.
-    *
-    * @default true
-    * @type {Boolean}
-    */
+     * Enables/disables the scrolling when clicking in a menu item.
+     * Disable if you'd like to handle the scrolling by your own.
+     *
+     * @default true
+     * @type {Boolean}
+     */
     clickToScroll: {
       type: Boolean,
       default: true,
     },
 
     /**
-    * The duration of the scroll animation when clicking to scroll
-    * is activated.
-    *
-    * @default 600
-    * @type {Number}
-    */
+     * The duration of the scroll animation when clicking to scroll
+     * is activated.
+     *
+     * @default 600
+     * @type {Number}
+     */
     duration: {
       type: Number,
       default: 600,
     },
 
     /**
-    * Defines if the plugin should track the section change when
-    * clicking an item to scroll to its section. If set to true,
-    * it will always keep track and change the active class to the
-    * current section while scrolling, if false, the active class
-    * will be immediately applied to the clicked menu item, ignoring
-    * the passed sections until the scrolling is over.
-    *
-    * @default false
-    * @type {Boolean}
-    */
+     * Defines if the plugin should track the section change when
+     * clicking an item to scroll to its section. If set to true,
+     * it will always keep track and change the active class to the
+     * current section while scrolling, if false, the active class
+     * will be immediately applied to the clicked menu item, ignoring
+     * the passed sections until the scrolling is over.
+     *
+     * @default false
+     * @type {Boolean}
+     */
     alwaysTrack: {
       type: Boolean,
       default: false,
     },
 
     /**
-    * Your custom easing value for the click to scroll functionality.
-    * It must be a string with 4 values separated by commas in a
-    * cubic bezier format.
-    *
-    * @default '.5,0,.35,1'
-    * @type {String}
-    */
+     * Your custom easing value for the click to scroll functionality.
+     * It must be a string with 4 values separated by commas in a
+     * cubic bezier format.
+     *
+     * @default '.5,0,.35,1'
+     * @type {String}
+     */
     bezierEasingValue: {
       type: String,
       default: '.5,0,.35,1',
@@ -125,10 +124,10 @@ export default {
 
   computed: {
     /**
-    * Computes the bezier easing string value into an array.
-    *
-    * @return {Array.<string>}
-    */
+     * Computes the bezier easing string value into an array.
+     *
+     * @return {Array.<string>}
+     */
     cubicBezierArray() {
       return this.bezierEasingValue.split(',');
     },
@@ -166,11 +165,11 @@ export default {
 
   methods: {
     /**
-    * Will be called when scrolling event is triggered to handle the addition of the active class
-    * in the current section item and fire the change event.
-    *
-    * @param {Object} event Scroll event.
-    */
+     * Will be called when scrolling event is triggered to handle the addition of the active class
+     * in the current section item and fire the change event.
+     *
+     * @param {Object} event Scroll event.
+     */
     onScroll(event) {
       this.currentItem = this.getItemInsideWindow();
 
@@ -194,21 +193,24 @@ export default {
 
       [].forEach.call(this.items, (item) => {
         const target = document.getElementById(item.hash.substr(1));
-        const isScreenPastSection = window.pageYOffset >= this.getOffsetTop(target) - this.offset;
-        const isScreenBeforeSectionEnd = window.pageYOffset <
-          (this.getOffsetTop(target) - this.offset) + target.offsetHeight;
+        if (target) {
+          const isScreenPastSection = window.pageYOffset >= this.getOffsetTop(target) - this.offset;
+          const isScreenBeforeSectionEnd = window.pageYOffset <
+            (this.getOffsetTop(target) - this.offset) + target.offsetHeight;
 
-        if (this.exact && isScreenPastSection && isScreenBeforeSectionEnd) currentItem = item;
-        if (!this.exact && isScreenPastSection) currentItem = item;
+          if (this.exact && isScreenPastSection && isScreenBeforeSectionEnd) currentItem = item;
+          if (!this.exact && isScreenPastSection) currentItem = item;
+        }
+
       });
 
       return currentItem;
     },
 
     /**
-    * Sets the list of menu items, adding or removing the click listener depending on the
-    * clickToScroll prop.
-    */
+     * Sets the list of menu items, adding or removing the click listener depending on the
+     * clickToScroll prop.
+     */
     initScrollactiveItems() {
       this.items = this.$el.querySelectorAll('.scrollactive-item');
 
@@ -235,10 +237,10 @@ export default {
     },
 
     /**
-    * Handles the scrolling when clicking a menu item.
-    *
-    * @param {Object} event The click event.
-    */
+     * Handles the scrolling when clicking a menu item.
+     *
+     * @param {Object} event The click event.
+     */
     handleClick(event) {
       event.preventDefault();
 
@@ -315,11 +317,11 @@ export default {
     },
 
     /**
-    * Gets the top offset position of an element in the document.
-    *
-    * @param  {Element} element
-    * @return {Number}
-    */
+     * Gets the top offset position of an element in the document.
+     *
+     * @param  {Element} element
+     * @return {Number}
+     */
     getOffsetTop(element) {
       let yPosition = 0;
       let nextElement = element;
@@ -342,4 +344,5 @@ export default {
     },
   },
 };
+
 </script>
